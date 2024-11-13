a41cadcb703881b0bae5f7188aa585ebd1494941
Miguel <miguel.gf.130@gmail.com>
2024-11-12 22:03:32 -0600

    feat(assets): Se agregan nuevos assets de tipo animacion

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,4 +1,7 @@
 class AppAssets {
   static const animationSplash = 'assets/animations/splash.json';
+  static const animationSuccess = 'assets/animations/success.json';
+  static const animationSuccessJump = 'assets/animations/success_jump.json';
+  static const animationQrLoading = 'assets/animations/qr_loading.json';
   static const imgFlutter = 'assets/images/logo_flutter.png';
 }
